4edfd9533d92e2035144597560f4f79181b1ee99
Joanatas Fernandes Barbosa <94496092+joanatasFernandes@users.noreply.github.com>
2022-12-19 20:29:55 -0300

    SK-001 Classe cursos

---
+++ src/oo/composicao/CursoTeste.java
@@ -0,0 +1,11 @@
+package oo.composicao;
+
+public class CursoTeste {
+    Aluno aluno1 = new Aluno("Jonathan");
+    Aluno aluno2 = new Aluno("Arthur");
+    Aluno aluno3 = new Aluno("Luiza");
+    Curso curso1 = new Curso("Ciencias da computação");
+    Curso curso2 = new Curso("Analise e desenvolvimento");
+    Curso curso3 = new Curso("Odontologia");
+    
+}
